839dd6a0bfb9dbcd444db3cd92fd73cc131b3a74
pol-metra1 <61022814+pol-metra1@users.noreply.github.com>
2025-11-17 14:03:22 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,6 +25,8 @@ apt -y install php-json
 apt -y install php-opcache
 apt -y install php-readline
 apt -y install php-mcrypt
+apt -y install php-snmp
+apt -y install php-bcmath
 a2enconf php-fpm.conf
 service php-fpm reload
 systemctl restart apache2
